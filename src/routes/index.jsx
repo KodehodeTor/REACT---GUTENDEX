dfea873e1225618fa49df7c3c4555fa8bb8d2d3a
KodehodeTor <torjarand.26pg@gmail.com>
2026-09-23 12:39:07 +0200

    Added folder router and index.jsx with basic routing implemented.

---
+++ src/routes/index.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+
+const router = createVrowserRouter ([
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {
+                index:true,
+                // element: <Home/> 
+            },
+            {
+                path:"books",
+                // element: <Books />
+            },
+            {
+                path:"books/:id",
+                // element: <BookDetails /> 
+            },
+        {    path:"favorites",
+            // element: <BookFavorites />,
+        },
+        ],
+    },
+    {
+        path: "*"
+        element: <h1>404</h1>
+    },
+])
+
+export { router }
